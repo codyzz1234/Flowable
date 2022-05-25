2cca9d7fd627cd5755834700bc6e4857a03b90f1
cody <kenleybeast1@gmail.com>
2022-05-25 20:56:44 +0800

    First Commit

---
+++ src/main/java/com/FlowableTutorialSpringBoot/example/FlowableIntegration/FlowableIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.FlowableTutorialSpringBoot.example.FlowableIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowableIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FlowableTutorialSpringBoot/example/FlowableIntegration/FlowableIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FlowableTutorialSpringBoot.example.FlowableIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
